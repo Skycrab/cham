e65883e8a2b0e96e640b706d3c59067a7f473ec6
skycrab <skycrab68@163.com>
2015-08-03 11:41:10 +0800

    cham Run and Stop

--- cham/smain.go
+++ cham/smain.go
@@ -4,15 +4,29 @@ import (
 	"time"
 )
 
-func mainStart(service *Service, args ...interface{}) Dispatch {
-	return func(session int32, source Address, ptype uint8, args ...interface{}) []interface{} {
-		return NORET
-	}
-}
+const (
+	CHAM_STOP uint8 = iota
+)
 
 var Main *Service
 
 var DTimer *Timer
+
+var stop chan NULL
+
+func mainStart(service *Service, args ...interface{}) Dispatch {
+	return func(session int32, source Address, ptype uint8, args ...interface{}) []interface{} {
+		switch ptype {
+		case CHAM_STOP:
+			stop <- NULLVALUE
+		}
+		return NORET
+	}
+}
+
+func Run() {
+	<-stop
+}
 
 func init() {
 	Main = NewService("main", mainStart)
